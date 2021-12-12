69817116b45a3cee53c80b3412602f83b9ae5c29
Lovro <lovro.napotnik@scv.si>
2021-12-12 18:13:59 +0100

    Header tabs component done

--- components/HeaderTabs.js
+++ components/HeaderTabs.js
@@ -1,16 +1,40 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
 
 export default function HeaderTabs() {
-    return (
-        <View>
-            <HeaderButton />
-
-            {/* HeaderButton */}
-        </View>
-    )
+  const [activeTab, setActiveTab] = useState("Dostava");
+  return (
+    <View style={{ flexDirection: "row", alignSelf: "center" }}>
+      <HeaderButton
+        text="Dostava"
+        btnColor="black"
+        textColor="white"
+        activeTab={activeTab}
+        setActiveTab={setActiveTab}
+      />
+      <HeaderButton
+        text="Prevzem"
+        btnColor="white"
+        textColor="black"
+        activeTab={activeTab}
+        setActiveTab={setActiveTab}
+      />
+    </View>
+  );
 }
 
-const HeaderButton = () => (
-    <Text>Delivery</Text>
-)
+const HeaderButton = (props) => (
+  <TouchableOpacity
+    style={{
+      backgroundColor: props.activeTab == props.text ? "black" : "white",
+      paddingVertical: 6,
+      paddingHorizontal: 16,
+      borderRadius: 30,
+    }}
+    onPress={() => props.setActiveTab(props.text)}
+  >
+    <Text style={{ color: props.activeTab == props.text ? "white" : "black", fontSize: 15, fontWeight: "900" }}>
+      {props.text}
+    </Text>
+  </TouchableOpacity>
+);
