278037f57bc0171706f25f6f52b747d5f074963c
Lovro <lovro.napotnik@scv.si>
2021-12-12 18:37:17 +0100

    Search bar

--- screens/home.js
+++ screens/home.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { View, Text, SafeAreaView } from 'react-native'
 import HeaderTabs from '../components/HeaderTabs';
+import SearchBar from '../components/SearchBar';
 import GlobalStyles from '../src/GlobalStyles';
 
 export default function Home() {
     return (
-        <SafeAreaView style={GlobalStyles.droidSafeArea}>
+        <SafeAreaView style={{backgroundColor: "#eee", flex: 1, paddingTop: 40}}>
+            <View style={{backgroundColor: "white", padding: 15}}> 
             <HeaderTabs />
+            <SearchBar />
+            </View> 
         </ SafeAreaView>
     );
 }
